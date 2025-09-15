99f63e39b083042898243a700d5e8f1a44d70e91
Stephanie Dixon <stephanie.dixon921@gmail.com>
2025-09-15 16:02:44 -0500

    add registration fields

--- frontend/src/components/pages/Register.tsx
+++ frontend/src/components/pages/Register.tsx
@@ -5,6 +5,7 @@ import TextInput, { MessageLevel } from "../shared/TextInput";
 import { register } from "../../api/Authorization";
 import { RegisterResponse } from "../../api/Authorization";
 import { Link, useNavigate } from 'react-router-dom';
+import { Notification, NotificationType, setSettings, SettingsResponse } from "../../api/Settings";
 
 interface RegisterProps {
     handleNavigate: (page: Page) => void;
@@ -15,13 +16,21 @@ interface RegisterFormData {
     username: string;
     password: string;
     confirmPassword: string;
+    email: Notification;
+    darkMode: boolean;
 }
 
 const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
     const [formData, setFormData] = useState<RegisterFormData>({
         username: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        email: {
+            type: NotificationType.EMAIL,
+            contactInfo: '',
+            allowContact: false
+        },
+        darkMode: false
     });
     const [message, setMessage] = useState<string>('');
     const [passwordMessage, setPasswordMessage] = useState<string>('');
@@ -31,7 +40,15 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
 
 
     useEffect(() => {
-        setFormData({username: '', password: '', confirmPassword: ''});
+        setFormData({username: '',
+             password: '',
+            confirmPassword: '',
+            email: {
+                type: NotificationType.EMAIL,
+                contactInfo: '',
+                allowContact: false
+            }, 
+            darkMode: false});
         setPasswordMessage("");
         setPasswordMessageLevel(MessageLevel.NONE)
     }, [])
@@ -48,6 +65,28 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
             updatePasswordMessage(formData.password, value);
         }
     };
+
+    const handleNotificationInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: {
+                ...prev,
+                contactInfo: value
+            }
+        }));
+    }
+
+    const handleNotificationToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = e.target;
+        setFormData(prev => ({
+          ...prev,
+          [name]: {
+            ...prev,
+            allowContact: checked
+          }
+        }));
+      };
 
     const updatePasswordMessage = (newPassword: string, newConfirmPassword: string) => {
         if(newConfirmPassword === undefined || newConfirmPassword.length < 1) {
@@ -68,17 +107,30 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
         setMessage('');
 
         try {
-            const data: RegisterResponse = await register(formData);
+            const data: RegisterResponse = await register({
+                username: formData.username,
+                password: formData.password
+            });
             if (data.success) {
                 setMessage('Registration successful');
                 setUsername(formData.username);
                 if (data.token) {
                     localStorage.setItem('authToken', data.token);
                 }
-                setTimeout(() => {
-                    handleNavigate('home');
-                    navigate("/home");
-                }, 1000);
+                const settingsData: SettingsResponse = await setSettings(localStorage.getItem('authToken'), {
+                    username: formData.username,
+                    darkMode: formData.darkMode,
+                    language: "en", //There's only support for english right now
+                    notifications: [formData.email]
+                });
+                if(settingsData.success) {
+                    setTimeout(() => {
+                        handleNavigate('home');
+                        navigate("/home");
+                    }, 1000);
+                } else {
+                    setMessage(data.message || 'Failed to save settings for user');
+                }
             } else {
                 setMessage(data.message || 'Registration failed');
             }
@@ -91,7 +143,9 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
     };
 
     const disableSubmit = () => {
-        return isLoading || formData.password !== formData.confirmPassword
+        return isLoading ||
+        formData.password !== formData.confirmPassword ||
+        (formData.username.length === 0 || formData.password.length === 0 || formData.email.contactInfo.length === 0);
     }
 
     return (
@@ -109,7 +163,7 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
                         onChange={handleInputChange}
                     />
                     <TextInput
-                        label="Password"
+                        label='Password'
                         id='password'
                         type='password'
                         value={formData.password}
@@ -118,7 +172,7 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
                         onChange={handleInputChange}
                     />
                     <TextInput
-                        label="Confirm Password"
+                        label='Confirm Password'
                         id='confirmPassword'
                         type='password'
                         value={formData.confirmPassword}
@@ -128,6 +182,25 @@ const Register: React.FC<RegisterProps> = ({ handleNavigate, setUsername }) => {
                         message={passwordMessage}
                         messageLevel={passwordMessageLevel}
                     />
+                    <TextInput //TODO need email validation
+                        label= 'Email'
+                        id='email'
+                        type='email'
+                        value={formData.email.contactInfo}
+                        placeholder='email'
+                        isLoading={isLoading}
+                        onChange={handleNotificationInputChange}
+                    />
+                    <div className="notification-preference">
+                        <label htmlFor="notifications">Email Notifications:</label>
+                        <input
+                            type="checkbox"
+                            id="notifications"
+                            checked={formData.email.allowContact}
+                            name='email'
+                            onChange={handleNotificationToggle}
+                        />
+                    </div>
                     <button type="submit" disabled={disableSubmit()} className="register-button">
                         {isLoading ? 'Registering...' : 'Register'}
                     </button>
